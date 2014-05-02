7aa9e78535d06c8aef0c6e4cd2b6bdc69571578d
JIAWEILONG <gary_0203@hotmail.com>
2014-05-02 08:17:07 +0800

    Update LoopConvert.cpp

--- LoopConvert.cpp
+++ LoopConvert.cpp
@@ -3,44 +3,31 @@
 #include <sstream>
 #include <iostream>
 #include <fstream>
-// Declares clang::SyntaxOnlyAction.
 #include "clang/Frontend/FrontendActions.h"
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
-// Declares llvm::cl::extrahelp.
 #include "llvm/Support/CommandLine.h"
-
 #include "clang/ASTMatchers/ASTMatchers.h"
 #include "clang/ASTMatchers/ASTMatchersMacros.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
-
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Rewrite/Frontend/Rewriters.h"
 #include "clang/Basic/SourceManager.h"
-
 #include "clang/Rewrite/Core/Rewriter.h"
 #include "clang/Parse/ParseAST.h"
-
 #include "clang/Basic/TargetOptions.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/ASTContext.h"
 #include "clang/AST/DeclBase.h"
 #include "clang/Lex/Lexer.h"
-
 using namespace clang::tooling;
 using namespace llvm;
 using namespace clang;
 using namespace clang::ast_matchers;
 using namespace std;
 
-
-// CommonOptionsParser declares HelpMessage with a description of the common
-// command-line options related to the compilation database and input files.
-// It's nice to have this help message in all tools.
 static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
-
-// A help message for this specific tool can be added afterwards.
 static cl::extrahelp MoreHelp("\nMore help text...");
 
 DeclarationMatcher fMatcher = 
@@ -50,7 +37,6 @@ class FunctionPrinter : public MatchFinder::MatchCallback {
 public:
   virtual void run(const MatchFinder::MatchResult &Result) {    
     if (const FunctionDecl *FD = Result.Nodes.getNodeAs<clang::FunctionDecl>("funcDecl"))
-      // FD->dumpColor();
       printf("FunctionName:%s\tType:%s\n",
         FD->getNameInfo().getAsString().c_str(), 
         FD->getResultType().getAsString().c_str());
@@ -64,7 +50,6 @@ class VarPrinter : public MatchFinder::MatchCallback {
 public:
   virtual void run(const MatchFinder::MatchResult &Result) {    
     if (const VarDecl *D = Result.Nodes.getNodeAs<clang::VarDecl>("varDecl"))
-      // D->dumpColor();
       printf("VariableName:%s\tType:%s\n",
         D->getDeclName().getAsString().c_str(),
         D->getType().getAsString().c_str());
@@ -164,12 +149,9 @@ int main(int argc, const char **argv) {
   // Finder.addMatcher(fMatcher, &Printer1);
   // Finder.addMatcher(vMatcher, &Printer2);
   // return (Tool.run(newFrontendActionFactory(&Finder)));
-
   CommonOptionsParser op(argc, argv);
   ClangTool Tool(op.getCompilations(), op.getSourcePathList());
   int result = Tool.run(newFrontendActionFactory<InstrumentAction>());
 
-  // Rewrite.getEditBuffer(Rewrite.getSourceMgr().getMainFileID()).write(errs());
-  
   return result;
 }
